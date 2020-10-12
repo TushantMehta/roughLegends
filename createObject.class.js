3fed4c21e752f003c2e20a80c5d68eadb6a03da8
Tushant Mehta <000435076@student.vcc.ca>
2020-10-12 10:19:04 -0700

    added initial files

---
+++ createObject.class.js
@@ -0,0 +1,12 @@
+class obj
+{
+    constructor(fname, lname, email, phoneNo, comapanyCode, EmployerOrNot)
+    {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.comapanyCode = comapanyCode;
+        this.EmployerOrNot = EmployerOrNot;
+    }
+}
